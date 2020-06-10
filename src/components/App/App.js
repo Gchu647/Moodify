6aa1d17cec74b55fff4e508a8abb47e39c35f34d
George <g7chu@Temps-MBP.hawaii.rr.com>
2020-06-09 23:34:31 -1000

    successfully get access data using client credential flow for Spotify

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
+import qs from 'qs';
+import axios from 'axios';
+import { headers, data } from '../../config';
 import logo from '../../logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    // Fetch token using client credentials flow  authorization
+    axios.post(
+      'https://accounts.spotify.com/api/token',
+      qs.stringify(data),
+      headers
+    )
+    .then( response => {
+      console.log('result', response.data);
+    })
+    .catch( err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
