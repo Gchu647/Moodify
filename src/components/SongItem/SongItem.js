81b55e15da649ebcd1f92c372956a48594517b93
George <g7chu@Temps-MBP.hawaii.rr.com>
2020-06-17 14:42:59 -1000

    display background image in SongItem

--- src/components/SongItem/SongItem.js
+++ src/components/SongItem/SongItem.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 const SongItem = props => {
   return (
-   <div>
-     <p>Song Item Smoke test</p>
-   </div>
+    <div className='song-container'>
+      <div className='album-image' style={sectionStyle}>
+		    <div className='song-score-container'>
+			    <p className='song-score'> 85</p>
+		    </div>
+	    </div>
+      <p className='song-title'>Falling</p>
+      <p className='song-artists'>Travor Daniel</p>
+    </div>
   );
 }
+var bgUrl = 'https://i.scdn.co/image/ab67616d00001e02bf01fd0986a195d485922167';
+
+var sectionStyle = {
+  width: '300px',
+  height: '300px',
+  backgroundImage: `url(${bgUrl})`,
+  backgroundColor: 'yellow'
+};
 
 export default SongItem;
