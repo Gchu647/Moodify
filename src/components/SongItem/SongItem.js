afed9d5725628757cca1b438be1e41a6edc78ed8
George <g7chu@Temps-MBP.hawaii.rr.com>
2020-06-17 16:18:23 -1000

    css songItem

--- src/components/SongItem/SongItem.js
+++ src/components/SongItem/SongItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './SongItem.css'
 
 const SongItem = props => {
   return (
@@ -8,17 +9,25 @@ const SongItem = props => {
 			    <p className='song-score'> 85</p>
 		    </div>
 	    </div>
-      <p className='song-title'>Falling</p>
-      <p className='song-artists'>Travor Daniel</p>
+      <div className='song-container-bottom'>
+        <div className='song-container-bottom-left'>
+          <p className='song-title'>Falling</p>
+          <p className='song-artists'>Travor Daniel</p>
+        </div>
+        <div className='song-container-bottom-right'>
+          <i class="fa fa-play-circle" aria-hidden="true"></i>
+        </div>
+      </div>
     </div>
   );
 }
 var bgUrl = 'https://i.scdn.co/image/ab67616d00001e02bf01fd0986a195d485922167';
 
 var sectionStyle = {
-  width: '300px',
-  height: '300px',
+  width: '100%',
+  height: '221px',
   backgroundImage: `url(${bgUrl})`,
+  backgroundSize: 'contain',
   backgroundColor: 'yellow'
 };
 
