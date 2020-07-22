cf1db1670dbf9855abfab88c99813ba46ace7b1b
George <g7chu@Temps-MBP.hawaii.rr.com>
2020-07-21 20:50:14 -1000

    added content to WelcomeModal

--- src/components/WelcomeModal/WelcomeModal.js
+++ src/components/WelcomeModal/WelcomeModal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Modal, Button } from 'rsuite';
 import 'rsuite/dist/styles/rsuite-default.css';
-// import './WelcomeModal.css';
+import './WelcomeModal.css';
 
 const WelcomeModal = props => {
   const {
@@ -10,18 +10,21 @@ const WelcomeModal = props => {
   } = props;
 
   return(
-    <div className='welcome-modal'>
-      <Modal size='md' show={showModal} onHide={close}>
-      <Modal.Body>
-        <p>Welcome Modal smoke test.</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={close} appearance="primary">
-          Close
-        </Button>
-      </Modal.Footer>
+    <Modal size='md' show={showModal} onHide={close}>
+      <div className='welcome-modal-content'>
+        <Modal.Body>
+          <h1>Welcome to Moodify!</h1>
+          <p>This a music recommendation app based on your mood. The musics recommended are scored from a scale of 0 to 100, with 0 being very sad, and 100 being very happy.</p>
+          <p>You can also search up songs or artists you know to checkout their mood scores.</p>
+          <p>This app only shows song previews, so if you like a song, then you will have checkout the full length song on either Spotify or Youtube.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={close} appearance="primary">
+            Close
+          </Button>
+        </Modal.Footer>
+      </div>
     </Modal>
-    </div>
   )
 }
 
