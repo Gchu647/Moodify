b6f5f959db04e5b743cf0a5608dc2b455aa61610
George <g7chu@Temps-MBP.hawaii.rr.com>
2020-07-22 02:46:55 -1000

    added to Spotify link to WelcomeModal

--- src/components/WelcomeModal/WelcomeModal.js
+++ src/components/WelcomeModal/WelcomeModal.js
@@ -13,8 +13,9 @@ const WelcomeModal = props => {
     <Modal size='md' show={showModal} onHide={close}>
       <div className='welcome-modal-content'>
         <Modal.Body>
-          <h1>Welcome to Moodify!</h1>
-          <p>This app recommends you songs based on your mood. The songs recommended are scored from a scale of 0 to 100, with 0 being very sad, and 100 being very happy.</p>
+          <h3>Welcome to Moodify!</h3>
+          <p>This app uitilizes the <a href='https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-features/' target='_blank'>Spotify API</a> to recommend music to you based on your mood. </p>
+          <p>The songs recommended are scored from a scale of 0 to 100, with 0 being very sad, and 100 being very happy.</p>
           <p>You can also search up songs or artists you know to check out their mood scores.</p>
           <p>This app only shows song previews, so if you like a song, then you will have to listen to the full length song on either Spotify or Youtube.</p>
           <p>P.S: The filter features on the left doesn't work in search</p>
